d23d3bb4d60a9c51560332410d25986626f75a8e
Bimofr <smarie-c@e3r1p9.42.fr>
2014-11-17 20:12:33 +0100

    phase

---
+++ app/controllers/phase_controller.rb
@@ -0,0 +1,19 @@
+class PhaseController < ApplicationController
+	def create_view
+	end
+
+	def create
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def delete
+	end
+end
---
+++ test/decorators/phase_decorator_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PhaseDecoratorTest < Draper::TestCase
+end
