6413a688c8c6e4fdf9e223f2c5ecd0567cb065ba
Ibrahim KERKEB <ikerkeb@e2r1p2.42.fr>
2014-11-25 20:35:52 +0100

    css d'ibrahim kerkerb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
+//= require foundation
+$(document).foundation();
